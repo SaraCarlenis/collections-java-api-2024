36484c4a03e0f69620a420a2602b8b56a8837a3e
SaraCarlenis <sara.hurtado.corte@hotmail.com>
2024-07-07 20:47:30 -0300

    Resolvendo desafio Lista Tarefas
    Signed-off-by: SaraCarlenis <sara.hurtado.corte@hotmail.com>

--- src/list/OperacoesBasicas/CarinhoDeCompras.java
+++ src/list/OperacoesBasicas/CarinhoDeCompras.java
@@ -1,4 +1,36 @@
 package list.OperacoesBasicas;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class CarinhoDeCompras {
-}
+        private List<Item> carrinho = new ArrayList<>();
+
+        public void adicionarItem(String nome, double preco, int quantidade) {
+
+            carrinho.add(new Item(nome, preco, quantidade));
+        }
+
+        public void removerItem(String nome) {
+            for (Item i : carrinho) {
+                if (i.getNome().equalsIgnoreCase(nome)){
+                    carrinho.remove(i);
+                }
+            }
+
+        }
+        public double calcularValorTotal() {
+            double valorTotal = 0;
+            for (Item i : carrinho) {
+                valorTotal = valorTotal + i.getPreco() * i.getQuantidade();
+            }
+            return valorTotal;
+        }
+        public void exibirItens() {
+            for (Item i : carrinho) {
+                System.out.println("nome: " + i.getNome() + " preço: " + i.getPreco() + " quantidade: " + i.getQuantidade());
+            }
+        }
+    }
+
+
--- src/list/OperacoesBasicas/Item.java
+++ src/list/OperacoesBasicas/Item.java
@@ -1,4 +1,26 @@
 package list.OperacoesBasicas;
 
 public class Item {
+    String nome;
+    double preco;
+    int quantidade;
+
+    public Item(String nome, double preco, int quantidade) {
+        this.nome = nome;
+        this.preco = preco;
+        this.quantidade = quantidade;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public double getPreco() {
+        return preco;
+    }
+
+    public int getQuantidade() {
+        return quantidade;
+    }
 }
+
