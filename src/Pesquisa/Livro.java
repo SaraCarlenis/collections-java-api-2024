d49ac77fe4c6c7aab346feb2773556d72f715238
SaraCarlenis <sara.hurtado.corte@hotmail.com>
2024-07-08 20:39:03 -0300

    Catalogo de Livros
    Signed-off-by: SaraCarlenis <sara.hurtado.corte@hotmail.com>

---
+++ src/Pesquisa/Livro.java
@@ -0,0 +1,36 @@
+package Pesquisa;
+
+public class Livro {
+    //atributos
+    private String titulo;
+    private String autor;
+    private int anoPublicacao;
+
+    //constructor para receber todos os atributos
+    public Livro(String titulo, String autor, int anoPublicacao) {
+        this.titulo = titulo;
+        this.autor = autor;
+        this.anoPublicacao = anoPublicacao;
+    }
+    //adicionamos o metodo getters
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public String getAutor() {
+        return autor;
+    }
+
+    public int getAnoPublicacao() {
+        return anoPublicacao;
+    }
+    //adicionamos o toString
+    @Override
+    public String toString() {
+        return "Livro{" +
+                "titulo='" + titulo + '\'' +
+                ", autor='" + autor + '\'' +
+                ", anoPublicacao=" + anoPublicacao +
+                '}';
+    }
+}
